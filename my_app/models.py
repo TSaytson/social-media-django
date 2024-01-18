cd50d08dd01b80566fe8e394eed43156a9258809
Thiago Saytson <thiagosaytson@poli.ufrj.br>
2024-01-18 14:56:20 -0300

    [features] Post verbose_name translations and users relationship

--- my_app/models.py
+++ my_app/models.py
@@ -1,8 +1,17 @@
-from unittest.util import _MAX_LENGTH
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Post(models.Model):
-  title = models.CharField(max_length=15)
-  content = models.TextField()
+  title = models.CharField(max_length=15, verbose_name='Título')
+  subtitle = models.CharField(max_length=30, null=True, blank=True, verbose_name='Subtítulo')
+  content = models.TextField(verbose_name='Conteúdo')
+  user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, verbose_name='Autor')
+
+  def __str__(self) -> str:
+    return self.title
+  
+  class Meta:
+    verbose_name = 'Artigo'
+    # verbose_name_plural = 'Artigos'
